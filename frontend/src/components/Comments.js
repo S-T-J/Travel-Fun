806d3a2aced81e50912572436b18a7e2256fe67e
jbaker825 <jbaker825@gmail.com>
2021-09-29 16:19:50 -0500

    change

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+
 
 const Comments = (props) => {
+
+    let [comment, setComment] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(`Send ${comment} to api.js then routes.js then db` )
+    }
+
     return (
         <div>
             Comments {props.match.params.threadId}
             <form onSubmit={handleSubmit}>
-                <input onChange={e => setTitle(e.target.value)} type="text" placeholder="Title" />
-                <input onChange={e => setThread(e.target.value)} type="text" placeholder="Thread" />
+               
+                <input onChange={e => setComment(e.target.value)} type="text" placeholder="Comment " />
                 <button>Submit</button>
             </form>
         </div>
