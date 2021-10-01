2aab3405f215031d2ca060e6726e9c303d43ed30
jbaker825 <jbaker825@gmail.com>
2021-10-01 10:03:26 -0500

    css

--- frontend/src/components/NewThread.js
+++ frontend/src/components/NewThread.js
@@ -1,30 +1,38 @@
-import { useState } from 'react';
-import axios from 'axios'
-import actions from '../api';
+import { useState } from "react";
+import axios from "axios";
+import actions from "../api";
 
 function NewThread(props) {
+  let [title, setTitle] = useState("");
+  let [thread, setThread] = useState("");
 
-    let [title, setTitle] = useState('')
-    let [thread, setThread] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let res = await actions.createNewThread({ title, thread });
+    props.history.push("/all-threads");
+  };
 
-
-    const handleSubmit = async e => {
-        e.preventDefault()
-        let res = await actions.createNewThread({ title, thread })
-        props.history.push('/all-threads')
-    }
-
-
-    return (
-        <div>
-            <label>New Thread</label>
-            <form onSubmit={handleSubmit}>
-                <input onChange={e => setTitle(e.target.value)} type="text" placeholder="Title" />
-                <input onChange={e => setThread(e.target.value)} type="text" placeholder="Thread" />
-                <button>Submit</button>
-            </form>
-        </div>
-    );
+  return (
+    <div id="new-thread" className="new-thread-div">
+      <form className="new-thread-form" onSubmit={handleSubmit}>
+        <h1>New Thread</h1>
+        <label>Title:</label>
+        <input
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          id="input1"
+        />
+        <label>Text (optional):</label>
+        <input
+          onChange={(e) => setThread(e.target.value)}
+          type="text"
+          placeholder="Text (optional)"
+          id="input2"
+        />
+        <button className="new-thread-button">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default NewThread;
