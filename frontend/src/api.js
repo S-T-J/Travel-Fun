b42ee44e2b8979576b5d15315239d9c179eac6d0
sarebro <sarebro77@gmail.com>
2021-10-04 17:41:34 -0500

    added upload photo to comments

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,101 +1,70 @@
-import axios from "axios";
+import axios from 'axios';
 
 let SERVER_URL =
-  process.env.NODE_ENV === "development"
-    ? `http://localhost:5000/api`
-    : `https://travel-bug-ironhack.herokuapp.com/api`;
+	process.env.NODE_ENV === 'development'
+		? `http://localhost:5000/api`
+		: `https://travel-bug-ironhack.herokuapp.com/api`;
 
 const createHeader = () => {
-  //Sends my token to the backend
-  return {
-    headers: { authorization: `Bearer ${localStorage.getItem("token")}` },
-  };
+	//Sends my token to the backend
+	return {
+		headers: { authorization: `Bearer ${localStorage.getItem('token')}` }
+	};
 };
 
 //THIS IS WHERE WE CONNECT TO THE BACKEND >> OUR FRONT END ROUTES
 const actions = {
-  getAllPosts: async () => {
-    return await axios.get(`${SERVER_URL}/all-posts`, createHeader());
-  },
-  getTopThreads: async () => {
-    return await axios.get(`${SERVER_URL}/top-threads`, createHeader());
-  },
-  getThreadInfo: async (threadId) => {
-    return await axios.get(`${SERVER_URL}/thread/${threadId}`, createHeader());
-  },
-  getAllThreads: async () => {
-    return await axios.get(`${SERVER_URL}/all-threads`, createHeader());
-  },
-  getAllComments: async (threadId) => {
-    console.log(threadId);
-    return await axios.get(
-      `${SERVER_URL}/all-comments/${threadId}`,
-      createHeader()
-    );
-  },
-  createNewPost: async ({ title, post }) => {
-    return await axios.post(
-      `${SERVER_URL}/new-post`,
-      { title, post },
-      createHeader()
-    );
-  },
-  createNewThread: async ({ title, text, image }) => {
-    return await axios.post(
-      `${SERVER_URL}/new-thread`,
-      { title, text, image },
-      createHeader()
-    );
-  },
+	getAllPosts: async () => {
+		return await axios.get(`${SERVER_URL}/all-posts`, createHeader());
+	},
+	getTopThreads: async () => {
+		return await axios.get(`${SERVER_URL}/top-threads`, createHeader());
+	},
+	getThreadInfo: async (threadId) => {
+		return await axios.get(`${SERVER_URL}/thread/${threadId}`, createHeader());
+	},
+	getAllThreads: async () => {
+		return await axios.get(`${SERVER_URL}/all-threads`, createHeader());
+	},
+	getAllComments: async (threadId) => {
+		console.log(threadId);
+		return await axios.get(`${SERVER_URL}/all-comments/${threadId}`, createHeader());
+	},
+	createNewPost: async ({ title, post }) => {
+		return await axios.post(`${SERVER_URL}/new-post`, { title, post }, createHeader());
+	},
+	createNewThread: async ({ title, text, image }) => {
+		return await axios.post(`${SERVER_URL}/new-thread`, { title, text, image }, createHeader());
+	},
 
-  createNewComment: async ({ text, threadId }) => {
-    console.log(text, threadId);
-    return await axios.post(
-      `${SERVER_URL}/new-comment`,
-      { text, threadId },
-      createHeader()
-    );
-  },
+	createNewComment: async ({ text, threadId, image }) => {
+		console.log(text, threadId, image);
+		return await axios.post(`${SERVER_URL}/new-comment`, { text, threadId, image }, createHeader());
+	},
 
-  upVote: async (commentId) => {
-    return await axios.post(
-      `${SERVER_URL}/upvote`,
-      { commentId },
-      createHeader()
-    );
-  },
+	upVote: async (commentId) => {
+		return await axios.post(`${SERVER_URL}/upvote`, { commentId }, createHeader());
+	},
 
-  downVote: async (commentId) => {
-    return await axios.post(
-      `${SERVER_URL}/downvote`,
-      { commentId },
-      createHeader()
-    );
-  },
+	downVote: async (commentId) => {
+		return await axios.post(`${SERVER_URL}/downvote`, { commentId }, createHeader());
+	},
 
-  upVoteThread: async (threadId) => {
-    return await axios.post(
-      `${SERVER_URL}/upvote-thread`,
-      { threadId },
-      createHeader()
-    );
-  },
+	upVoteThread: async (threadId) => {
+		return await axios.post(`${SERVER_URL}/upvote-thread`, { threadId }, createHeader());
+	},
 
-  downVoteThread: async (threadId) => {
-    return await axios.post(
-      `${SERVER_URL}/downvote-thread`,
-      { threadId },
-      createHeader()
-    );
-  },
+	downVoteThread: async (threadId) => {
+		return await axios.post(`${SERVER_URL}/downvote-thread`, { threadId }, createHeader());
+	},
 
-  authenticate: async (user) => {
-    let res = await axios.post(`${SERVER_URL}/authenticate`, user);
-    localStorage.setItem("token", res.data.token);
-  },
-  getUser: async () => {
-    return await axios.get(`${SERVER_URL}/get-user`, createHeader());
-  },
+	authenticate: async (user) => {
+		let res = await axios.post(`${SERVER_URL}/authenticate`, user);
+		localStorage.setItem('token', res.data.token);
+	},
+	getUser: async () => {
+		return await axios.get(`${SERVER_URL}/get-user`, createHeader());
+	}
 };
 
 export default actions;
--- frontend/src/components/NewComment.js
+++ frontend/src/components/NewComment.js
@@ -1,27 +1,60 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import actions from '../api';
 
 const Comments = (props) => {
-	let [comment, setComment] = useState('');
+	let [ comment, setComment ] = useState('');
+	let [ image, setImage ] = useState('');
+	let [ disabled, setDisabled ] = useState(false);
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		let res = await actions.createNewComment({ text: comment, threadId: props.match.params.threadId });
+		let res = await actions.createNewComment({ text: comment, threadId: props.match.params.threadId, image });
 		// console.log(`Send ${comment} to api.js then routes.js then db`);
 
-		console.log("res.data", res); //Response from backend with your new comment.{ tetxt:'b;ah', _id: '345fgdgsdg'}
+		console.log('res.data', res); //Response from backend with your new comment.{ tetxt:'b;ah', _id: '345fgdgsdg'}
 
-		let newComments = [...props.comments]; //Copy of all previous comments on the page
+		let newComments = [ ...props.comments ]; //Copy of all previous comments on the page
 		newComments.unshift(res.data); //Push ouur new commennt into he copy
 		props.setComments(newComments); //Set to state.  Replace old list with new list so we see it wihtoutt reload
-		setComment("")
+		setComment('');
 	};
+
+	async function uploadPhoto(e) {
+		setDisabled(true);
+		const [ file ] = e.target.files;
+		const reader = new FileReader();
+		const formData = new FormData();
+		// let file = e.target.files[0]
+		console.log(typeof file, file);
+		formData.append('file', file);
+		formData.append('upload_preset', 'zs3vfefq');
+		let res = await axios.post(
+			'https://iron-cors-anywhere.herokuapp.com/https://api.cloudinary.com/v1_1/dxv7j2sj6/upload',
+			formData
+		);
+		console.log(res.data);
+		setDisabled(false);
+		setImage(res.data.secure_url);
+	}
 
 	return (
 		<div className="allcomments-form">
 			<form onSubmit={handleSubmit}>
-				<input className="input" value={comment} onChange={(e) => setComment(e.target.value)} type="text" placeholder="Comment " />
-				<button className="allcomments-button">Submit</button>
+				<input
+					required
+					className="input"
+					value={comment}
+					onChange={(e) => setComment(e.target.value)}
+					type="text"
+					placeholder="Comment "
+				/>
+				<input type="file" onChange={uploadPhoto} />
+				<div className="comments-form-control">
+					<button disabled={disabled} className="allcomments-button">
+						Submit
+					</button>
+				</div>
 			</form>
 		</div>
 	);
