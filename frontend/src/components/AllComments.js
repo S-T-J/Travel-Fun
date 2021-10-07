270a5fa95a8f46adcd254c030dfa565a96ca6d02
jbaker825 <jbaker825@gmail.com>
2021-10-07 15:25:25 -0500

    css

--- frontend/src/components/AllComments.js
+++ frontend/src/components/AllComments.js
@@ -5,108 +5,115 @@ import EachComment from "./EachComment";
 import EachReply from "./EachReply";
 
 function AllComments(props) {
-  const [comments, setComments] = useState([]);
-  const [thread, setThread] = useState({ title: "" });
-  const [replies, setReplies] = useState([]);
+	const [comments, setComments] = useState([]);
+	const [thread, setThread] = useState({ title: "" });
+	const [replies, setReplies] = useState([]);
 
-  useEffect(async () => {
-    let res = await actions.getAllComments(props.match.params.threadId);
-    //setComments(res.data.reverse());
-    console.log(res.data, "this.one");
-    let threadInfo = await actions.getThreadInfo(props.match.params.threadId);
-    console.log(props.match.params.threadId, threadInfo);
-    setThread(threadInfo.data);
+	useEffect(async () => {
+		let res = await actions.getAllComments(props.match.params.threadId);
+		//setComments(res.data.reverse());
+		console.log(res.data, "this.one");
+		let threadInfo = await actions.getThreadInfo(props.match.params.threadId);
+		console.log(props.match.params.threadId, threadInfo);
+		setThread(threadInfo.data);
 
-    let depth = 0;
+		let depth = 0;
 
-    function findReplies(commentId, obj) {
-      let replies = res.data
-        .reverse()
-        .filter((eachComment) => eachComment.commentId == commentId);
-      obj["replies"] = replies;
+		function findReplies(commentId, obj) {
+			let replies = res.data
+				.reverse()
+				.filter((eachComment) => eachComment.commentId == commentId);
+			obj["replies"] = replies;
 
-      for (let reply of replies) {
-        findReplies(reply._id, reply);
-        depth++;
-      }
-      //
-      if (depth == res.data.length) {
-        console.log(obj, "...");
-        setReplies(obj.replies);
-        //   console.log(JSON.stringify(obj))
-      }
-    }
+			for (let reply of replies) {
+				findReplies(reply._id, reply);
+				depth++;
+			}
+			//
+			if (depth == res.data.length) {
+				console.log(obj, "...");
+				setReplies(obj.replies);
+				//   console.log(JSON.stringify(obj))
+			}
+		}
 
-    findReplies(null, { name: "first" });
-  }, []);
+		findReplies(null, { name: "first" });
+	}, []);
 
-  // const ShowReplies = () => {
-  // 	let all = []
-  // 	replies.map(each => {
-  // 		all.push(<div>{each.text}</div>)
-  // 		return each.replies.map(each => {
-  // 			all.push( <div>{each.text}</div> )
+	// const ShowReplies = () => {
+	// 	let all = []
+	// 	replies.map(each => {
+	// 		all.push(<div>{each.text}</div>)
+	// 		return each.replies.map(each => {
+	// 			all.push( <div>{each.text}</div> )
 
-  // 		})
-  // 	})
-  // 	return all
-  // }
+	// 		})
+	// 	})
+	// 	return all
+	// }
 
-  const ShowReplies = () => {
-    let all = [];
+	const ShowReplies = () => {
+		let all = [];
 
-    function digDeeper(num, replies) {
-      num++;
-      for (let each of replies) {
-        all.push(
-          <div style={{ marginLeft: num * 100 + "px" }}>
-            {each.text}
+		function digDeeper(num, replies) {
+			num++;
+			for (let each of replies) {
+				all.push(
+					<>
+						<div style={{ marginLeft: num * 50 + "px", marginTop: num * 0 + "px" }}>
+							{each.text}
 
-            <EachReply eachComment={each} key={each._id} {...props} />
-          </div>
-        );
-        console.log(each, num);
-        if (each.replies.length > 0) {
-          digDeeper(num, each.replies);
-        }
-      }
-    }
+							<EachReply eachComment={each} key={each._id} {...props} />
 
-    digDeeper(0, replies);
-    console.log("is this called", all, replies);
-    return all;
-  };
 
-  //useEffect(async () => {}, [ props.match.params.threadId ]);
+							{num % 2 === 0 ? <vr style={{ backgroundColor: 'white' }}></vr> : <vr style={{ backgroundColor: '#eceeef' }}></vr>}
 
-  const ShowComments = (props) => {
-    return comments.map((eachComment, i) => {
-      return (
-        <EachComment
-          eachComment={eachComment}
-          i={i}
-          setComments={setComments}
-          comments={comments}
-          key={eachComment._id}
-          {...props}
-        />
-      );
-    });
-  };
+						</div>
 
-  return (
-    <div className="allcomments-page">
-      <h2>{thread?.title}</h2>
-      <h3>{thread?.text}</h3>
-      <img src={thread?.image} width="250" height="auto" />
-      <p className="allcomments-header">Comments</p>
-      <div className="allcomments-setcomments">
-        {/* <NewComment {...props} comments={comments} setComments={setComments} /> */}
-        {/* <ShowComments {...props} /> */}
-        <ShowReplies {...props} />
-      </div>
-    </div>
-  );
+					</>
+				);
+				console.log(each, num);
+				if (each.replies.length > 0) {
+					digDeeper(num, each.replies);
+				}
+			}
+		}
+
+		digDeeper(0, replies);
+		console.log("is this called", all, replies);
+		return all;
+	};
+
+	//useEffect(async () => {}, [ props.match.params.threadId ]);
+
+	const ShowComments = (props) => {
+		return comments.map((eachComment, i) => {
+			return (
+				<EachComment
+					eachComment={eachComment}
+					i={i}
+					setComments={setComments}
+					comments={comments}
+					key={eachComment._id}
+					{...props}
+				/>
+			);
+		});
+	};
+
+	return (
+		<div className="allcomments-page">
+			<h2>{thread?.title}</h2>
+			<h3>{thread?.text}</h3>
+			<img src={thread?.image} width="250" height="auto" />
+			<p className="allcomments-header">Comments</p>
+			<div className="allcomments-setcomments">
+				{/* <NewComment {...props} comments={comments} setComments={setComments} /> */}
+				{/* <ShowComments {...props} /> */}
+				<ShowReplies {...props} />
+			</div>
+		</div>
+	);
 }
 
 export default AllComments;
