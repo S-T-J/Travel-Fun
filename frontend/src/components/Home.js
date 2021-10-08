4e8e8a2e3405a493cf900015c10d63d3b81d8156
jbaker825 <jbaker825@gmail.com>
2021-10-08 08:35:18 -0500

    css

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,71 +4,71 @@ import { Link } from "react-router-dom";
 // import './index.css';
 
 const Home = () => {
-  let [topThreads, setTopThreads] = useState([]);
+    let [topThreads, setTopThreads] = useState([]);
 
-  useEffect(async () => {
-    const res = await actions.getTopThreads();
-    setTopThreads(res.data);
-  }, []);
+    useEffect(async () => {
+        const res = await actions.getTopThreads();
+        setTopThreads(res.data);
+    }, []);
 
-  const ShowTopThreads = () =>
-    topThreads.map((eachThread) => {
-      return (
-        <div key={eachThread._id}>
-          <div className="thread-info-section">
-            <h2>
-              <Link to={`/thread/${eachThread._id}`}>{eachThread.title}</Link>
-            </h2>
-          </div>
-          <div className="user-section">
-            <span className="user-image-section">
-              {eachThread.userId && (
-                <img className="user-image" src={eachThread.userId.imageUrl} />
-              )}
-            </span>
-            <span className="user-name-section">
-              {eachThread.userId && eachThread.userId.name}
-            </span>
-          </div>
-          <p>{eachThread.text}</p>
-          <div className="vote-section">
-            <span className="upvote-section">
-              <span>
-                <img
-                  // onClick={(e) => upVoteThread(eachThread._id, i)}
-                  className="img-vote"
-                  src="https://www.vippng.com/png/full/116-1160623_up-arrow-png-up-arrow-png-white.png"
-                />
-              </span>
-              <span className="vote-count">{eachThread.upvote}</span>
-            </span>
-            <span className="downvote-section">
-              <span>
-                <img
-                  // onClick={(e) => downVoteThread(eachThread._id, i)}
-                  className="img-vote downvote"
-                  src="https://www.vippng.com/png/full/116-1160623_up-arrow-png-up-arrow-png-white.png"
-                />
-              </span>
-              <span className="vote-count">{eachThread.downvote}</span>
-            </span>
-          </div>
-          <hr />
-        </div>
-      );
-    });
+    const ShowTopThreads = () =>
+        topThreads.map((eachThread) => {
+            return (
+                <div key={eachThread._id}>
+                    <div className="thread-info-section">
+                        <h2>
+                            <Link to={`/thread/${eachThread._id}`}>{eachThread.title}</Link>
+                        </h2>
+                    </div>
+                    <p>{eachThread.text}</p>
+                    <div className="user-section">
+                        <span className="user-image-section">
+                            {eachThread.userId && (
+                                <img className="user-image" src={eachThread.userId.imageUrl} />
+                            )}
+                        </span>
+                        <span className="user-name-section">
+                            {eachThread.userId && eachThread.userId.name}
+                        </span>
+                    </div>
+                    <div className="vote-section">
+                        <span className="upvote-section">
+                            <span>
+                                <img
+                                    // onClick={(e) => upVoteThread(eachThread._id, i)}
+                                    className="img-vote"
+                                    src="https://www.vippng.com/png/full/116-1160623_up-arrow-png-up-arrow-png-white.png"
+                                />
+                            </span>
+                            <span className="vote-count">{eachThread.upvote}</span>
+                        </span>
+                        <span className="downvote-section">
+                            <span>
+                                <img
+                                    // onClick={(e) => downVoteThread(eachThread._id, i)}
+                                    className="img-vote downvote"
+                                    src="https://www.vippng.com/png/full/116-1160623_up-arrow-png-up-arrow-png-white.png"
+                                />
+                            </span>
+                            <span className="vote-count">{eachThread.downvote}</span>
+                        </span>
+                    </div>
+                    <hr />
+                </div>
+            );
+        });
 
-  return (
-    <>
-      <div
-        className="home-headerimage"
-        style={{ backgroundImage: `url(headerimage.jpg)` }}
-      ></div>
-      <div className="home-showtopthreads">
-        <h2 className="home-topfivetitle">Top 5 Threads:</h2>
-        <ShowTopThreads />
-      </div>
-      {/* <br></br>
+    return (
+        <>
+            <div
+                className="home-headerimage"
+                style={{ backgroundImage: `url(headerimage.jpg)` }}
+            ></div>
+            <div className="home-showtopthreads">
+                <h2 className="home-topfivetitle">Top 5 Threads:</h2>
+                <ShowTopThreads />
+            </div>
+            {/* <br></br>
             <br></br>
             <br></br>
             <br></br>
@@ -91,8 +91,8 @@ const Home = () => {
                     </p>
                 </div>
             </footer> */}
-    </>
-  );
+        </>
+    );
 };
 
 export default Home;
